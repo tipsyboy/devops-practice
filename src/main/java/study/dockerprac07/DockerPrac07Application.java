edc57ec423250ea7d58255a9b0196dc7c8d3543e
tipsyboy <gudah13@naver.com>
2025-09-10 10:14:41 +0900

    test

---
+++ src/main/java/study/dockerprac07/DockerPrac07Application.java
@@ -0,0 +1,13 @@
+package study.dockerprac07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPrac07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPrac07Application.class, args);
+    }
+
+}
