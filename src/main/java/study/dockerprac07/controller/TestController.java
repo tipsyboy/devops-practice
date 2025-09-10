0ff4fd1cdc8f9270d596c3c2ab45c914699b0ef6
tipsyboy <gudah13@naver.com>
2025-09-10 12:38:13 +0900

    testtset

--- src/main/java/study/dockerprac07/controller/TestController.java
+++ src/main/java/study/dockerprac07/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping
     public String test() {
-        return "sibal!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+        return "sibal!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111!";
     }
 }
